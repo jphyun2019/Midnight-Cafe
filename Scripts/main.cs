f09c6c10abb28233aff6f1ed2644dc11ba8a8b99
jphyun2019 <99457892+jphyun2019@users.noreply.github.com>
2023-02-17 14:33:35 -0500

    kj

--- Scripts/main.cs
+++ Scripts/main.cs
@@ -9,15 +9,12 @@ using UnityEngine;
 
 public enum step
 {
-
     Grinding,
     Pressing,
     Brewing,
     PouringMilk,
     Steaming,
     Combining
-
-
 }
 
 public class main : MonoBehaviour
@@ -25,11 +22,12 @@ public class main : MonoBehaviour
 
     public AudioSource music;
     public AudioSource beat;
-    public float starttimer = -2000;
+    public double starttimer;
     public bool playing = false;
     public int bpm;
 
     public int nextBeat = 0;
+    public double beatCount;
     public step currentStep;
     public int grounds;
 
@@ -37,13 +35,9 @@ public class main : MonoBehaviour
     public int[] rhythm;
 
 
-
-
     void Start()
     {
         currentStep = step.Grinding;
-
-
     }
     // Update is called once per frame
     void Update()
@@ -51,7 +45,7 @@ public class main : MonoBehaviour
 
         if (!playing)
         {
-            starttimer += Time.deltaTime * 1000;
+            starttimer += (double)Time.deltaTime;
             if (starttimer > 0)
             {
                 music.Play(0);
@@ -62,17 +56,28 @@ public class main : MonoBehaviour
         {
             double audioTime = ((double)music.timeSamples / music.clip.frequency) * 1000;
 
-            Debug.Log(audioTime);
+            beatCount = (((double)music.timeSamples / music.clip.frequency ) * bpm) / 60;
 
-            if (audioTime > nextBeat)
+
+            if (beatCount > nextBeat)
             {
                 beat.Play(0);
-                nextBeat += (60000 / (bpm*4));
+                nextBeat += (60 / (bpm));
             }
 
             if(currentStep == step.Grinding)
             {
-                
+                if (Input.GetButtonDown("j")|Input.GetButtonDown("k"))
+                {
+                    if(beatCount%1 < 0.2)
+                    {
+                        Debug.Log("good: " + beatCount % 1);
+                    }
+                    else
+                    {
+                        Debug.Log("bad: " + beatCount % 1);
+                    }
+                }
             }
 
 
