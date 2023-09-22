f0733e908055ea7f15a960eb3a794f00fb87d5a4
Jonathan Hyun <jonathan.hyun@duke.edu>
2023-09-21 22:43:51 -0400

    its back

--- Scripts/main.cs
+++ Scripts/main.cs
@@ -24,14 +24,21 @@ public class main : MonoBehaviour
 
     public double nextBeat = 0;
     public double beatCount;
+    public double audioTime = 0;
+    public double gameTime;
+    public double difference;
+    public double maxDifference;
+
     public step currentStep;
     public int grounds;
     public double latency;
     public double missRange;
     public double perfectRange;
 
+
+
     public List<double> rhythm = new List<double>();
-    public List<(double, double)> holds = new List<(double, double)>();
+
 
     void Start()
     {
@@ -52,9 +59,19 @@ public class main : MonoBehaviour
         }
         else
         {
-            double audioTime = ((double)music.timeSamples / music.clip.frequency) * 1000;
 
-            beatCount = (((double)music.timeSamples / music.clip.frequency ) * bpm) / 60;
+            gameTime += Time.deltaTime;
+            audioTime = ((double)music.timeSamples / music.clip.frequency);
+            difference = audioTime - gameTime;
+
+            if(Mathf.Abs((float)difference) > maxDifference)
+            {
+                maxDifference = Mathf.Abs((float)difference);
+            }
+
+
+
+            beatCount = (audioTime * (double)bpm) / 60;
 
 
             if (beatCount > nextBeat)
@@ -69,7 +86,7 @@ public class main : MonoBehaviour
 
 
 
-            if (Input.GetButtonDown("j") | Input.GetButtonDown("k"))
+            if (Input.GetButtonDown("j") | Input.GetButtonDown("k") | Input.GetButtonDown("d") | Input.GetButtonDown("f"))
             {
                 //Debug.Log((beatCount-0.175) % 1);
                 if ((rhythm.Count == 0) && ((beatCount % 1 < (missRange + latency)) || (beatCount % 1 > ((1 - missRange) + latency))))
@@ -87,15 +104,23 @@ public class main : MonoBehaviour
                         beat.Play(0);
                     }
                     double[] tempRhythm = { };
-                    (double, double)[] tempHold = { };
+
                     if (currentStep == step.Grinding)
                     {
-                        tempRhythm = new double[] { 1,2,3};
+                        tempRhythm = new double[] { 1,2,3, 4, 5, 6, 7};
                     }
                     if (currentStep == step.Pressing)
                     {
-                        tempRhythm = new double[] { 2, 3};
-                        tempHold = new (double, double)[] {(0, 1) };
+                        tempRhythm = new double[] { 2, 3,4,6,7};
+
+                    }
+                    if (currentStep == step.Brewing)
+                    {
+                        tempRhythm = new double[] {0.5, 1, 1.5, 2, 2.5, 3, 3.5 };
+                    }
+                    if (currentStep == step.PouringMilk)
+                    {
+                        tempRhythm = new double[] {1.5, 3, 4, 5.5, 7, 7.5};
                     }
 
                     foreach (double d in tempRhythm)
@@ -112,6 +137,7 @@ public class main : MonoBehaviour
                     {
                         Debug.Log("miss");
                         rhythm.RemoveAt(0);
+                        rhythm.Clear();
                     }
                     else
                     {
